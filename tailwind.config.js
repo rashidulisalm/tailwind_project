ddd1b0ad4b9bcd9c1ac2d1cebcf25ef962318cbe
Rashed@USG_PC <rashidulislam.robi@gmail.com>
2024-11-16 20:14:54 +0600

    tailwind-project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*"],
+  content: ["./*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
 }
 
